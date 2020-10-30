abe29c5cbe8eaef747ce1cf6a7abd855c3e383cd
GabrielaRamos23 <gabrielaramosch23@gmail.com>
2020-10-30 17:52:48 -0500

    subiendo GET and POST

---
+++ home.php
@@ -0,0 +1,8 @@
+<?php 
+
+$nombre = htmlentities($_GET['nombre']);
+$fechanac = htmlentities($_GET['fechanac']);
+$dni = htmlentities($_GET['dni']);
+echo "Imprimiendo" .$nombre. " ".$fechanac. " ".$dni;
+
+?>
